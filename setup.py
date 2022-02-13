9d63d8bd9c59ddd7d79c698a665fb4c44cc0570f
David Vegh <david.andras.vegh+github@gmail.com>
2022-02-13 20:42:27 +0100

    setup.py: fix version cause PR contains new features

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md") as fp:
 
 setup(
     name="ipyvizzu",
-    version="0.3.2",
+    version="0.4.0",
     description="Jupyter notebook integration for vizzu",
     long_description=long_description,
     long_description_content_type="text/markdown",
