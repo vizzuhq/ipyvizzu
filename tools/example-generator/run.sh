e0a3f859e8e75a419f4e4c1b268698a537f7a844
Laszlo Simon <simon@vizzuhq.com>
2022-03-27 00:33:46 +0100

    tools/example-generator: generates CSV data files from vizzu-lib repo too.

--- tools/example-generator/run.sh
+++ tools/example-generator/run.sh
@@ -2,17 +2,28 @@
 
 ./sparse-clone-vizzu-lib.sh
 
-outdir=../../docs/examples
-staticExamples=$( find vizzu-lib/test/integration/test_cases/web_content/sample_static/ -name "*.mjs" )
-animatedExamples=$( find vizzu-lib/test/integration/test_cases/web_content/templates/ -name "*.mjs" )
+docsDir=../../docs
+examplesDir=${docsDir}/examples
+testDir=vizzu-lib/test/integration
+testcaseDir=${testDir}/test_cases/web_content
 
-mkdir -p ${outdir}/static
-mkdir -p ${outdir}/animated
+staticExamples=$( find ${testcaseDir}/sample_static/ -name "*.mjs" )
+animatedExamples=$( find ${testcaseDir}/templates/ -name "*.mjs" )
+datafiles="chart_types_eu.mjs infinite_data.mjs music_industry_history_1.mjs tutorial.mjs"
+
+mkdir -p ${docsDir}/data
+mkdir -p ${examplesDir}/static
+mkdir -p ${examplesDir}/animated
+
+for datafile in $datafiles; do
+  echo "Processing $datafile"
+  node vizzu-lib/tools/js2csv/js2csv.js "../../test/integration/test_data/${datafile}" ${docsDir}/data/$(basename ${datafile%.mjs}.csv)
+done
 
 for example in $staticExamples; do
-  ./mjs2ipynb.sh $example ${outdir}/static/$(basename ${example%.mjs}.ipynb)
+  ./mjs2ipynb.sh $example ${examplesDir}/static/$(basename ${example%.mjs}.ipynb)
 done
 
 for example in $animatedExamples; do
-  ./mjs2ipynb.sh $example ${outdir}/animated/$(basename ${example%.mjs}.ipynb)
+  ./mjs2ipynb.sh $example ${examplesDir}/animated/$(basename ${example%.mjs}.ipynb)
 done
--- tools/example-generator/sparse-clone-vizzu-lib.sh
+++ tools/example-generator/sparse-clone-vizzu-lib.sh
@@ -2,4 +2,7 @@
 git clone --filter=blob:none --no-checkout --depth 1 https://github.com/vizzuhq/vizzu-lib.git
 cd vizzu-lib
 git sparse-checkout init --cone
-git sparse-checkout set test/integration/test_cases/web_content test/integration/test_data
+git sparse-checkout set \
+  test/integration/test_cases/web_content \
+  test/integration/test_data \
+  tools/js2csv
