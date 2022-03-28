4126b7efe852093c30594d38c5f950a2d70ecceb
David Vegh <david.andras.vegh+github@gmail.com>
2022-03-28 21:33:44 +0200

    make doc: add req, add path to git sparse

--- tools/example-generator/sparse-clone-vizzu-lib.sh
+++ tools/example-generator/sparse-clone-vizzu-lib.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 git clone --filter=blob:none --no-checkout --depth 1 https://github.com/vizzuhq/vizzu-lib.git
-cd vizzu-lib
-git sparse-checkout init --cone
-git sparse-checkout set \
+git -C vizzu-lib sparse-checkout init --cone
+git -C vizzu-lib sparse-checkout set \
   test/integration/test_cases/web_content \
   test/integration/test_data \
   tools/js2csv
+ls -R vizzu-lib
