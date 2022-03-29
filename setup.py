3e705b042c13c3296e9e3874e522389264ed2470
David Vegh <61405792+veghdev@users.noreply.github.com>
2022-03-29 16:25:48 +0200

    setup.py: version 0.7.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md") as fp:
 
 setup(
     name="ipyvizzu",
-    version="0.6.0",
+    version="0.7.0",
     description="ipyvizzu is the Jupyter Notebook integration of Vizzu.",
     long_description=long_description,
     long_description_content_type="text/markdown",
