7a1c74ff5050cea08adfd9b46f03602f2652e2be
David Vegh <david.andras.vegh+github@gmail.com>
2022-03-29 10:59:44 +0200

    docs: add custom nbconvert preprocessor

---
+++ tools/html-generator/preprocessor.py
@@ -0,0 +1,12 @@
+from nbconvert.preprocessors import Preprocessor
+import re
+
+
+class NbPreprocessor(Preprocessor):
+
+    def preprocess_cell(self, cell, resources, index):
+
+        if 'source' in cell and cell.cell_type == "markdown":
+            cell.source = re.sub(r"\[([^]]*)\]\(([^)]*)\.ipynb\)",r"[\1](\2.html)",cell.source)
+
+        return cell, resources
