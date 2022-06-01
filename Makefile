39868722444f70552cbbba7267f865230faa0067
David Vegh <david.andras.vegh+github@gmail.com>
2022-06-01 15:06:47 +0200

    makefile, release wf: handle multiple package

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-.PHONY: install dev clean check test format check-format lint
+.PHONY: install dev dev-req clean check test format check-format lint release release-build release-validate
 
 VIRTUAL_ENV = .venv
 DEV_BUILD_FLAG = $(VIRTUAL_ENV)/DEV_BUILD_FLAG
@@ -6,26 +6,33 @@ NOTEBOOKS = $(shell find docs -type f -name '*.ipynb' -not -path '*/.ipynb_check
 
 install:
 	$(VIRTUAL_ENV)/bin/python setup.py install
+	$(VIRTUAL_ENV)/bin/python setup.py install --ipyvizzu
+	$(VIRTUAL_ENV)/bin/python setup.py install --stpyvizzu
 
-dev: $(DEV_BUILD_FLAG)
+dev-req:
+	$(VIRTUAL_ENV)/bin/pip-compile --upgrade dev-requirements.in
+
+dev: $(DEV_BUILD_FLAG) install
 
 $(DEV_BUILD_FLAG):
 	python3 -m venv $(VIRTUAL_ENV)
-	$(VIRTUAL_ENV)/bin/python setup.py install
+	$(VIRTUAL_ENV)/bin/pip install pip==22.0.4
 	$(VIRTUAL_ENV)/bin/pip install -r dev-requirements.txt
 	$(VIRTUAL_ENV)/bin/ipython kernel install --name ".venv" --user
 	touch $(DEV_BUILD_FLAG)
 
 clean:
 	-rm -rf $(VIRTUAL_ENV)
+	-rm -rf build
+	-rm -rf dist
+	-rm -rf *.egg-info
 
-requirements:
-	$(VIRTUAL_ENV)/bin/pip-compile --upgrade dev-requirements.in
 
 doc: $(NOTEBOOKS:.ipynb=.html)
 
 %.html: %.ipynb $(DEV_BUILD_FLAG)
 	cd tools/html-generator; ../../$(VIRTUAL_ENV)/bin/jupyter nbconvert --Exporter.preprocessors=preprocessor.NbPreprocessor --to html --template classic --execute ../../$<
+
 
 check: check-format lint test
 
@@ -45,3 +52,15 @@ lint: $(DEV_BUILD_FLAG)
 		--disable missing-module-docstring \
 		--disable too-few-public-methods \
 		src tests tools
+
+
+release-build:
+	$(VIRTUAL_ENV)/bin/python setup.py sdist bdist_wheel
+	$(VIRTUAL_ENV)/bin/python setup.py sdist bdist_wheel --ipyvizzu
+	$(VIRTUAL_ENV)/bin/python setup.py sdist bdist_wheel --stpyvizzu
+
+release-validate:
+	$(VIRTUAL_ENV)/bin/python -m twine check dist/*.tar.gz dist/*.whl
+
+release: release-build release-validate
+	
--- setup.py
+++ setup.py
@@ -1,87 +1,98 @@
 #!/usr/bin/env python3
 
 from setuptools import setup
+import sys
 
 
 with open("README.md") as fp:
     long_description = fp.read()
 
+
 version = "1.0.0"
 
-setup(
-    name="pyvizzu",
-    version=version,
-    description="pyvizzu is the Python integration of Vizzu.",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    license="Apache 2",
-    packages=['pyvizzu'],
-    package_dir={'pyvizzu': 'src/pyvizzu'},
-    package_data={'pyvizzu': ['templates/*.js']},
-    python_requires='>=3.6',
-    install_requires=["pandas"],
-    url="https://github.com/vizzuhq/ipyvizzu",
-    project_urls={
-        "Documentation": "https://ipyvizzu.vizzuhq.com/",
-        "Source": "https://github.com/vizzuhq/ipyvizzu",
-        "Tracker": "https://github.com/vizzuhq/ipyvizzu/issues",
-    },
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: Apache Software License",
-        "Operating System :: OS Independent",
-        "Environment :: Console",
-    ],
-)
 
-setup(
-    name="ipyvizzu",
-    version=version,
-    description="ipyvizzu is the Jupyter Notebook integration of Vizzu.",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    license="Apache 2",
-    packages=['ipyvizzu'],
-    package_dir={'ipyvizzu': 'src/ipyvizzu'},
-    package_data={'ipyvizzu': ['templates/*.js']},
-    python_requires='>=3.6',
-    install_requires=["IPython", f"pyvizzu=={version}"],
-    url="https://github.com/vizzuhq/ipyvizzu",
-    project_urls={
-        "Documentation": "https://ipyvizzu.vizzuhq.com/",
-        "Source": "https://github.com/vizzuhq/ipyvizzu",
-        "Tracker": "https://github.com/vizzuhq/ipyvizzu/issues",
-    },
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: Apache Software License",
-        "Operating System :: OS Independent",
-        "Environment :: Console",
-    ],
-)
+select = "pyvizzu"
+if "--ipyvizzu" in sys.argv:
+    select = "ipyvizzu"
+    sys.argv.remove("--ipyvizzu")
+elif "--stpyvizzu" in sys.argv:
+    select = "stpyvizzu"
+    sys.argv.remove("--stpyvizzu")
 
-setup(
-    name="streamlit-pyvizzu",
-    version=version,
-    description="streamlit-pyvizzu is the Streamlit integration of Vizzu.",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    license="Apache 2",
-    packages=['stpyvizzu'],
-    package_dir={'stpyvizzu': 'src/stpyvizzu'},
-    package_data={'stpyvizzu': ['templates/*.js']},
-    python_requires='>=3.6',
-    install_requires=["streamlit", f"pyvizzu=={version}"],
-    url="https://github.com/vizzuhq/ipyvizzu",
-    project_urls={
-        "Documentation": "https://ipyvizzu.vizzuhq.com/",
-        "Source": "https://github.com/vizzuhq/ipyvizzu",
-        "Tracker": "https://github.com/vizzuhq/ipyvizzu/issues",
-    },
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: Apache Software License",
-        "Operating System :: OS Independent",
-        "Environment :: Console",
-    ],
-)
+
+if select == "pyvizzu":
+    setup(
+        name="pyvizzu",
+        version=version,
+        description="pyvizzu is the Python integration of Vizzu.",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        license="Apache 2",
+        packages=["pyvizzu"],
+        package_dir={"pyvizzu": "src/pyvizzu"},
+        package_data={"pyvizzu": ["templates/*.js"]},
+        python_requires=">=3.6",
+        install_requires=["pandas"],
+        url="https://github.com/vizzuhq/ipyvizzu",
+        project_urls={
+            "Documentation": "https://ipyvizzu.vizzuhq.com/",
+            "Source": "https://github.com/vizzuhq/ipyvizzu",
+            "Tracker": "https://github.com/vizzuhq/ipyvizzu/issues",
+        },
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: Apache Software License",
+            "Operating System :: OS Independent",
+            "Environment :: Console",
+        ],
+    )
+elif select == "ipyvizzu":
+    setup(
+        name="ipyvizzu",
+        version=version,
+        description="ipyvizzu is the Jupyter Notebook integration of Vizzu.",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        license="Apache 2",
+        packages=["ipyvizzu"],
+        package_dir={"ipyvizzu": "src/ipyvizzu"},
+        python_requires=">=3.6",
+        install_requires=["IPython", f"pyvizzu=={version}"],
+        url="https://github.com/vizzuhq/ipyvizzu",
+        project_urls={
+            "Documentation": "https://ipyvizzu.vizzuhq.com/",
+            "Source": "https://github.com/vizzuhq/ipyvizzu",
+            "Tracker": "https://github.com/vizzuhq/ipyvizzu/issues",
+        },
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: Apache Software License",
+            "Operating System :: OS Independent",
+            "Environment :: Console",
+        ],
+    )
+elif select == "stpyvizzu":
+    setup(
+        name="streamlit-pyvizzu",
+        version=version,
+        description="streamlit-pyvizzu is the Streamlit integration of Vizzu.",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        license="Apache 2",
+        packages=["stpyvizzu"],
+        package_dir={"stpyvizzu": "src/stpyvizzu"},
+        python_requires=">=3.6",
+        install_requires=["streamlit", f"pyvizzu=={version}"],
+        url="https://github.com/vizzuhq/ipyvizzu",
+        project_urls={
+            "Documentation": "https://ipyvizzu.vizzuhq.com/",
+            "Source": "https://github.com/vizzuhq/ipyvizzu",
+            "Tracker": "https://github.com/vizzuhq/ipyvizzu/issues",
+        },
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: Apache Software License",
+            "Operating System :: OS Independent",
+            "Environment :: Console",
+        ],
+    )
