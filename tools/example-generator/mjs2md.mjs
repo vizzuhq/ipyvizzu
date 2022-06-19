cd551666c83e290c564e1a1d6eb373a41ed774da
David Vegh <david.andras.vegh+github@gmail.com>
2022-06-19 17:15:12 +0200

    docs: order tutorial pages

--- tools/example-generator/mjs2md.mjs
+++ tools/example-generator/mjs2md.mjs
@@ -33,7 +33,7 @@ kernelspec:
 title: ipyvizzu - Example
 ---
 
-**Note:** The data used in this example is available [here](https://github.com/vizzuhq/ipyvizzu/tree/gh-pages/docs/data). You can read more about the available types of data in the [Adding data](../../tutorial/data.ipynb) chapter.
+**Note:** The data used in this example is available [here](https://github.com/vizzuhq/ipyvizzu/tree/gh-pages/docs/data). You can read more about the available types of data in the [Adding data](../../tutorial/01_02_adding_data.ipynb) chapter.
 
 `
 		;
