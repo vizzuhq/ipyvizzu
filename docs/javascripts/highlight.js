7c7091fc26c01a58dbf1ef8afdbe9eabfad458a8
David Vegh <david.andras.vegh@gmail.com>
2022-12-22 10:12:01 +0100

    doc: fix reference highlight

--- docs/javascripts/highlight.js
+++ docs/javascripts/highlight.js
@@ -1,1 +1,8 @@
-hljs.highlightAll();
+document.addEventListener('DOMContentLoaded', (event) => {
+    document.querySelectorAll('pre code').forEach((el) => {
+        if (window.location.href.includes('/reference/ipyvizzu/')) {
+            el.classList.add('language-python');
+        }
+        hljs.highlightElement(el);
+    });
+});
