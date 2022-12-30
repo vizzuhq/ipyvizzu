66f715d65517bb2d9cf176544e4d4832677d2f19
David Vegh <david.andras.vegh@gmail.com>
2022-12-30 12:54:51 +0100

    doc: add story

---
+++ docs/examples/stories/titanic/titanic.js
@@ -0,0 +1,54 @@
+const csv2JsLoaded = import("../../../javascripts/csv2js.js");
+const mdChartLoaded = import("../../../javascripts/mdchart.js");
+
+Promise.all([csv2JsLoaded, mdChartLoaded]).then((results) => {
+  const Csv2Js = results[0].default;
+  const MdChart = results[1].default;
+
+  const csv2js = new Csv2Js();
+  const dataLoaded = csv2js.getData("./titanic.csv");
+
+  dataLoaded.then((data) => {
+    const mdchart = new MdChart(data, "./vizzu.js", "example");
+
+    mdchart.create([
+      {
+        anims: [
+          (chart) => {
+            return chart.animate({
+              config: {
+                x: "Count",
+                y: "Sex",
+                label: "Count",
+                title: "Passengers of the Titanic",
+              },
+            });
+          },
+          (chart) => {
+            return chart.animate({
+              config: {
+                x: ["Count", "Survived"],
+                label: ["Count", "Survived"],
+                color: "Survived",
+              },
+            });
+          },
+          (chart) => {
+            return chart.animate({
+              config: { x: "Count", y: ["Sex", "Survived"] },
+            });
+          },
+          (chart) => {
+            return chart.animate({
+              config: {
+                x: ["Count", "Sex", "Survived"],
+                y: null,
+                coordSystem: "polar",
+              },
+            });
+          },
+        ],
+      },
+    ]);
+  });
+});
---
+++ docs/examples/stories/titanic/titanic.md
@@ -0,0 +1,49 @@
+---
+csv_url: ./titanic.csv
+---
+
+# Passengers of the Titanic
+
+<div id="example_01"></div>
+
+```python
+import pandas as pd
+from ipyvizzu import Chart, Data, Config
+
+data_frame = pd.read_csv("./titanic.csv")
+data = Data()
+data.add_data_frame(data_frame)
+
+chart = Chart()
+
+chart.animate(data)
+
+chart.animate(
+    Config(
+        {
+            "x": "Count",
+            "y": "Sex",
+            "label": "Count",
+            "title": "Passengers of the Titanic",
+        }
+    )
+)
+chart.animate(
+    Config(
+        {
+            "x": ["Count", "Survived"],
+            "label": ["Count", "Survived"],
+            "color": "Survived",
+        }
+    )
+)
+chart.animate(Config({"x": "Count", "y": ["Sex", "Survived"]}))
+
+chart.animate(
+    Config(
+        {"x": ["Count", "Sex", "Survived"], "y": None, "coordSystem": "polar"}
+    )
+)
+```
+
+<script src="./titanic.js"></script>
