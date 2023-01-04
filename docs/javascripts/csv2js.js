7a3accc069b0f6e1b0ef0834535702fe0d0ae235
David Vegh <david.andras.vegh@gmail.com>
2023-01-04 15:59:03 +0100

    doc: use d3 v7

--- docs/javascripts/csv2js.js
+++ docs/javascripts/csv2js.js
@@ -7,41 +7,36 @@ class Csv2Js {
       const detectedDimensions = {};
       const data = { series: [], records: [] };
       // eslint-disable-next-line no-undef
-      d3.csv(
-        csv,
-        (row) => {
-          const record = [];
-          const keys = Object.keys(row);
-          for (const key of keys) {
-            const numValue = +row[key];
-            if (!isNaN(numValue)) {
-              record.push(numValue);
-            } else {
-              record.push(row[key]);
-              detectedDimensions[key] = true;
-            }
+      const csvLoaded = d3.csv(csv, (row) => {
+        const record = [];
+        const keys = Object.keys(row);
+        for (const key of keys) {
+          const numValue = +row[key];
+          if (!isNaN(numValue)) {
+            record.push(numValue);
+          } else {
+            record.push(row[key]);
+            detectedDimensions[key] = true;
           }
-          data.records.push(record);
-          return record;
-        },
-        (error, rows) => {
-          if (error) {
-            return reject(error);
-          }
-          for (let i = 0; i < rows.columns.length; i++) {
-            const key = rows.columns[i];
-            const series = {
-              name: key,
-              type:
-                dimensions.includes(key) || detectedDimensions[key]
-                  ? "dimension"
-                  : "measure",
-            };
-            data.series.push(series);
-          }
-          return resolve(data);
         }
-      );
+        data.records.push(record);
+        return row;
+      });
+
+      csvLoaded.then((header) => {
+        for (let i = 0; i < header.columns.length; i++) {
+          const key = header.columns[i];
+          const series = {
+            name: key,
+            type:
+              dimensions.includes(key) || detectedDimensions[key]
+                ? "dimension"
+                : "measure",
+          };
+          data.series.push(series);
+        }
+        return resolve(data);
+      });
     });
   }
 }
