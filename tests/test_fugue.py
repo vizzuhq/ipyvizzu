f465a2b331736fd04f8b4a480f982086f9da559f
Han Wang <hwang@lyft.com>
2023-02-28 06:52:46 +0000

    update

--- tests/test_fugue.py
+++ tests/test_fugue.py
@@ -1,35 +1,40 @@
 import fugue.api as fa  # type: ignore
 import pandas as pd  # type: ignore
+import unittest
 
 # register the extension, not needed in practical use
 import ipyvizzu.fugue  # noqa # pylint: disable-all
 
 
-def test_fugue_extension_preset():
-    """Test Fugue extension - preset"""
-    df = pd.DataFrame(dict(a=list("abcde"), b=range(5)))
-    fa.fugue_sql_flow(
-        """
-        SELECT * FROM df WHERE b<5
+class TestFugue(unittest.TestCase):
+    """
+    A class for testing Fugue integration.
+    """
 
-        OUTPUT USING vizzu:bar(x="a",y="b")
-    """,
-        df=df,
-    ).run()
+    def test_fugue_extension_preset(self):
+        """Test Fugue extension - preset"""
+        df = pd.DataFrame(dict(a=list("abcde"), b=range(5)))
+        fa.fugue_sql_flow(
+            """
+            SELECT * FROM df WHERE b<5
 
+            OUTPUT USING vizzu:bar(x="a",y="b")
+        """,
+            df=df,
+        ).run()
 
-def test_fugue_extension_timeline():
-    """Test Fugue extension - timeline"""
-    df = pd.DataFrame(dict(a=list("abcde"), b=range(5), c=[1, 1, 2, 2, 3]))
-    fa.fugue_sql_flow(
-        """
-        SELECT * FROM df WHERE b<5
+    def test_fugue_extension_timeline(self):
+        """Test Fugue extension - timeline"""
+        df = pd.DataFrame(dict(a=list("abcde"), b=range(5), c=[1, 1, 2, 2, 3]))
+        fa.fugue_sql_flow(
+            """
+            SELECT * FROM df WHERE b<5
 
-        OUTPUT USING vizzu:timeline_bar(
-            by="c",
-            config={"x":"b","y":"a",title="x %s"},
-            duration=0.3
-        )
-    """,
-        df=df,
-    ).run()
+            OUTPUT USING vizzu:timeline_bar(
+                by="c",
+                config={"x":"b","y":"a",title="x %s"},
+                duration=0.3
+            )
+        """,
+            df=df,
+        ).run()
